173c771452e76911527a4f2c8f4b5e4390ff31a8
HCJosue <149279025+HCJosue@users.noreply.github.com>
2024-05-05 17:13:41 -0400

    Add files via upload

---
+++ Pregunta4/WebForm1.aspx.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Pregunta4
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            String nombre="";
+            if (Request.QueryString["nombre"]!=null) {
+                nombre = Request.QueryString["nombre"].ToString();
+            }
+            String apellido = "";
+            if (Request.QueryString["apellido"] != null)
+            {
+                apellido = Request.QueryString["apellido"].ToString();
+            }
+            String edad = "";
+            if (Request.QueryString["edad"] != null)
+            {
+                edad = Request.QueryString["edad"].ToString();
+            }
+            Label2.Text = "Nombre: "+nombre;
+            Label3.Text = "Apellido: "+apellido;
+            Label4.Text = "Edad: " + edad;
+            
+        }
+    }
+}
